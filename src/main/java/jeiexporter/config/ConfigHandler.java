d89712eca18de8615bf0998efd80b7fc3c2e40d6
way2muchnoise <jens.baetens@live.be>
2016-03-31 14:03:38 +0200

    adds config classes

---
+++ src/main/java/jeiexporter/config/ConfigHandler.java
@@ -0,0 +1,58 @@
+package jeiexporter.config;
+
+import jeiexporter.reference.Reference;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.client.config.IConfigElement;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConfigHandler
+{
+    public static Configuration config;
+    private static File configDir;
+
+    public static void init(File configDir)
+    {
+        if (config == null)
+        {
+            configDir = new File(configDir, Reference.ID);
+            configDir.mkdir();
+            ConfigHandler.configDir = configDir;
+            config = new Configuration(new File(configDir, Reference.ID + ".cfg"));
+            loadConfig();
+        }
+    }
+
+    public static File getConfigDir()
+    {
+        return configDir;
+    }
+
+    @SubscribeEvent
+    public void onConfigChangedEvent(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.modID.equalsIgnoreCase(Reference.ID))
+        {
+            loadConfig();
+        }
+    }
+
+    private static void loadConfig()
+    {
+        if (config.hasChanged())
+            config.save();
+    }
+
+    @SuppressWarnings("unchecked")
+    public static List<IConfigElement> getConfigElements()
+    {
+        List<IConfigElement> list = new ArrayList<IConfigElement>();
+        list.addAll(new ConfigElement(config.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements());
+        return list;
+    }
+}
---
+++ src/main/java/jeiexporter/gui/ModGuiConfig.java
@@ -0,0 +1,19 @@
+package jeiexporter.gui;
+
+import jeiexporter.config.ConfigHandler;
+import jeiexporter.reference.Reference;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.fml.client.config.GuiConfig;
+
+public class ModGuiConfig extends GuiConfig
+{
+    public ModGuiConfig(GuiScreen guiScreen)
+    {
+        super(guiScreen,
+                ConfigHandler.getConfigElements(),
+                Reference.ID,
+                false,
+                false,
+                GuiConfig.getAbridgedConfigPath(ConfigHandler.config.toString()));
+    }
+}
