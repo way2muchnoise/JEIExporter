9b7b917e78be05fffbc614beb6da5e42475730bb
way2muchnoise <jens.baetens@live.be>
2016-04-09 22:47:26 +0200

    split twoWayMap into regToDisp and dispToReg

--- src/main/java/jeiexporter/json/TooltipJsonMap.java
+++ src/main/java/jeiexporter/json/TooltipJsonMap.java
@@ -2,7 +2,6 @@ package jeiexporter.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.stream.JsonWriter;
 import jeiexporter.config.ConfigHandler;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
@@ -15,34 +14,36 @@ import java.util.Map;
 
 public class TooltipJsonMap
 {
-    private static Map<String, String> twoWayMap = new HashMap<>();
+    private static Map<String, String> regToDisp = new HashMap<>();
+    private static Map<String, String> dispToReg = new HashMap<>();
 
     public static void clear()
     {
-        twoWayMap.clear();
+        regToDisp.clear();
+        dispToReg.clear();
     }
 
     public static void add(ItemStack itemStack)
     {
         String regName = itemStack.getItem().getRegistryName() + ":" + itemStack.getMetadata();
         String dispName = itemStack.getDisplayName();
-        twoWayMap.put(regName, dispName);
-        twoWayMap.put(dispName, regName);
+        regToDisp.put(regName, dispName);
+        dispToReg.put(dispName, regName);
     }
 
     public static void add(FluidStack fluidStack)
     {
         String regName = fluidStack.getFluid().getName();
         String dispName = fluidStack.getLocalizedName();
-        twoWayMap.put(regName, dispName);
-        twoWayMap.put(dispName, regName);
+        regToDisp.put(regName, dispName);
+        dispToReg.put(dispName, regName);
     }
 
-    public static void asJson(File location) throws IOException
+    public static void asJson(File location, Map<String, String> map) throws IOException
     {
         FileWriter writer = new FileWriter(location);
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        writer.write(gson.toJson(twoWayMap));
+        writer.write(gson.toJson(map));
         writer.flush();
         writer.close();
         clear();
@@ -50,6 +51,7 @@ public class TooltipJsonMap
 
     public static void saveAsJson() throws IOException
     {
-        asJson(new File(ConfigHandler.getConfigDir() + "/exports/tooltipMap.json"));
+        asJson(new File(ConfigHandler.getConfigDir() + "/exports/tooltipMap.json"), regToDisp);
+        asJson(new File(ConfigHandler.getConfigDir() + "/exports/lookupMap.json"), dispToReg);
     }
 }
